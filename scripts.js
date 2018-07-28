70d0f26e33f949eceb583235935d43f9d494f3af
Rachel Eiting <reiting1@gmail.com>
2018-07-28 11:19:07 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    //create empty array
+    var formArray = [];
+
+    $('.btn').click(function(event) {
+        event.preventDefault();
+        var formVal = {};
+        formVal.prodkey = $('#firstProdName').attr('name');
+        formVal.prodvalue = $('#firstProdName').val();
+        formVal.desckey = $('#firstProdDesc').attr('name');
+        formVal.descvalue = $('#firstProdDesc').val();
+        formVal.pricekey = $('#firstProdPrice').attr('name');
+        formVal.pricevalue = $('#firstProdPrice').val();
+
+        var secondFormVal = {};
+        secondFormVal.prodkey = $('#secondProdName').attr('name');
+        secondFormVal.prodvalue = $('#secondProdName').val();
+        secondFormVal.desckey = $('#secondProdDesc').attr('name');
+        secondFormVal.descvalue = $('#secondProdDesc').val();
+        secondFormVal.pricekey = $('#secondProdPrice').attr('name');
+        secondFormVal.pricevalue = $('#secondProdPrice').val();
+
+        var thirdFormVal = {};
+        thirdFormVal.prodkey = $('#thirdProdName').attr('name');
+        thirdFormVal.prodvalue = $('#thirdProdName').val();
+        thirdFormVal.desckey = $('#thirdProdDesc').attr('name');
+        thirdFormVal.descvalue = $('#thirdProdDesc').val();
+        thirdFormVal.pricekey = $('#thirdProdPrice').attr('name');
+        thirdFormVal.pricevalue = $('#thirdProdPrice').val();
+
+        formArray.push(formVal);
+        formArray.push(secondFormVal);
+        formArray.push(thirdFormVal);
+        
+        console.table(formArray);
+    })
+
+   
+})
