0f0019a7e0ec2904a4498bfe0edb648d1d15f476
Rachel Eiting <reiting1@gmail.com>
2018-07-30 18:09:40 -0500

    added comments

--- comparison.js
+++ comparison.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
     //console.log("Hello");
     //$('#result').append('Hello World');
-
+    //gets the JSON string stored in local storage and sets it to myStorage
     var myStorage = window.localStorage;
 
-
+    //takes the JSON from the storage and parses it so it will be an array an sets it to parsedArray
     var parsedArray = JSON.parse(myStorage.formArray);
+    //gets the key/value pairs from the first object in the array and puts them in
+    //the html as text
     $('#name1').text(parsedArray[0].prodvalue);
     $('#desc1').text(parsedArray[0].descvalue);
     $('#price1').text(parsedArray[0].pricevalue);
 
+    //gets the key/value pairs from the second object in the array and puts them in
+    //the html as text
     $('#name2').text(parsedArray[1].prodvalue);
     $('#desc2').text(parsedArray[1].descvalue);
     $('#price2').text(parsedArray[1].pricevalue);
 
+    //gets the key/value pairs from the third object in the array and puts them in
+    //the html as text
     $('#name3').text(parsedArray[2].prodvalue);
     $('#desc3').text(parsedArray[2].descvalue);
     $('#price3').text(parsedArray[2].pricevalue);
--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
     //create empty array
     var formArray = [];
-    if ($('body').hasClass('one')) {
+    //if ($('body').hasClass('one')) {
+    //run the following code when the submit button is clicked
     $('#submitButton').click(function() {
+        //create an empty object
         var formVal = {};
+        //gets the keys (attr) and the values (val) from the input fields.
+        //Puts them into keys and values in the formVal object so that it
+        //has all the keys and values for the first product. 
+        //takes the attribute assigned to the first input field and puts it in formVal as prodkey
         formVal.prodkey = $('#firstProdName').attr('name');
+        //takes the value input into the first input field and puts it in formVal as prodvalue
         formVal.prodvalue = $('#firstProdName').val();
         formVal.desckey = $('#firstProdDesc').attr('name');
         formVal.descvalue = $('#firstProdDesc').val();
         formVal.pricekey = $('#firstProdPrice').attr('name');
         formVal.pricevalue = $('#firstProdPrice').val();
 
+        //does the same as above for the second product
         var secondFormVal = {};
         secondFormVal.prodkey = $('#secondProdName').attr('name');
         secondFormVal.prodvalue = $('#secondProdName').val();
@@ -19,6 +27,7 @@ $(document).ready(function() {
         secondFormVal.pricekey = $('#secondProdPrice').attr('name');
         secondFormVal.pricevalue = $('#secondProdPrice').val();
 
+        //does the same as above for third product
         var thirdFormVal = {};
         thirdFormVal.prodkey = $('#thirdProdName').attr('name');
         thirdFormVal.prodvalue = $('#thirdProdName').val();
@@ -27,20 +36,19 @@ $(document).ready(function() {
         thirdFormVal.pricekey = $('#thirdProdPrice').attr('name');
         thirdFormVal.pricevalue = $('#thirdProdPrice').val();
 
+        //pushes formVal to the empty array
         formArray.push(formVal);
+        //pushes secondFormVal to the empty array
         formArray.push(secondFormVal);
+        //pushes thirdFormVal to the empty array
         formArray.push(thirdFormVal);
 
+        //takes the array with all 3 objects and stringifies it to create a JSON string.
+        //takes the JSON string and pushes it to local storage.
         localStorage.setItem("formArray", JSON.stringify(formArray));
-        
-        
-    //click
     })
-} else if ($('body').hasClass('two')) {
-    var myStorage = window.localStorage;
-    //$('#result').text(myStorage[0].prodkey.val());
-    //console.log(myStorage.getItem('formArray'));
-
-}
-    //final
+//} else if ($('body').hasClass('two')) {
+    //var myStorage = window.localStorage;
+//}
+    //final closing tag
 })
