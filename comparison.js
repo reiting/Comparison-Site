a68705d2bbdcf69b5512cdf02c8e2a229e628260
Rachel Eiting <reiting1@gmail.com>
2018-08-04 10:42:00 -0500

    comparison.js now uses an each loop to get the values and print them to the page

--- comparison.js
+++ comparison.js
@@ -1,31 +1,19 @@
 $(document).ready(function() {
-    //console.log("Hello");
-    //$('#result').append('Hello World');
+
     //gets the JSON string stored in local storage and sets it to myStorage
     var myStorage = window.localStorage;
-
-    //takes the JSON from the storage and parses it so it will be an array an sets it to parsedArray
+    
+    //takes the JSON from the storage and parses it so it will be an object and sets it to parsedArray
     var parsedArray = JSON.parse(myStorage.formArray);
-    //gets the key/value pairs from the first object in the array and puts them in
-    //the html as text
-    $('#name1').text(parsedArray[0].prodvalue);
-    $('#desc1').text(parsedArray[0].descvalue);
-    $('#price1').text(parsedArray[0].pricevalue);
 
-    //gets the key/value pairs from the second object in the array and puts them in
-    //the html as text
-    $('#name2').text(parsedArray[1].prodvalue);
-    $('#desc2').text(parsedArray[1].descvalue);
-    $('#price2').text(parsedArray[1].pricevalue);
-
-    //gets the key/value pairs from the third object in the array and puts them in
-    //the html as text
-    $('#name3').text(parsedArray[2].prodvalue);
-    $('#desc3').text(parsedArray[2].descvalue);
-    $('#price3').text(parsedArray[2].pricevalue);
-
-    //console.log(JSON.parse(localStorage.getItem('formArray')));
-    //$('#result').text(myStorage[0].prodkey.val());
-    //console.log(myStorage.getItem('formArray'));
+    //iterates through the array of objects and has a function with a counter (iterator) and the object
+    $.each( parsedArray, function( i, val ) {
+        //set the text of the id of name + 1 (because the array is 0 indexed) to be the value of prodvalue
+        $( "#name" + (i+1) ).text( "Mine is " + val.prodvalue + "." );
+        //set the text of the id of desc + 1 (because the array is 0 indexed) to be the value of descvalue
+        $( "#desc" + (i+1) ).text( "Mine is " + val.descvalue + "." );
+        //set the text of the id of price + 1 (because the array is 0 indexed) to be the value of pricevalue
+        $( "#price" + (i+1) ).text( "Mine is " + val.pricevalue + "." );
+      });
 })
 
