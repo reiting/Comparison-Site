66ce1e715f00cd841f538a177dce2f684b9f4f39
Rachel Eiting <reiting1@gmail.com>
2018-08-05 09:36:51 -0500

    added some styling to the comparison page and the ability to find the lowest price to the js

--- comparison.js
+++ comparison.js
@@ -9,11 +9,25 @@ $(document).ready(function() {
     //iterates through the array of objects and has a function with a counter (iterator) and the object
     $.each( parsedArray, function( i, val ) {
         //set the text of the id of name + 1 (because the array is 0 indexed) to be the value of prodvalue
-        $( "#name" + (i+1) ).text( "Mine is " + val.prodvalue + "." );
+        $( '#name' + (i+1) ).text( val.prodvalue);
         //set the text of the id of desc + 1 (because the array is 0 indexed) to be the value of descvalue
-        $( "#desc" + (i+1) ).text( "Mine is " + val.descvalue + "." );
+        $( '#desc' + (i+1) ).text( val.descvalue );
         //set the text of the id of price + 1 (because the array is 0 indexed) to be the value of pricevalue
-        $( "#price" + (i+1) ).text( "Mine is " + val.pricevalue + "." );
-      });
+        $( '#price' + (i+1) ).text( val.pricevalue );
+      });  
+      
+      //on button click
+      $('button').click(function(){
+          //set var min to be the lowest price value
+        var min = Math.min.apply(null, parsedArray.map(function(a){return a.pricevalue;}));
+        //iterate over the class of price
+          $('.price').each(function() {
+              //if the textContent (the number) is equal to min
+            if(this.textContent == min) {
+                //give it the class of better-price
+                $(this).addClass('better-price');
+            }
+          })
+      })
 })
 
