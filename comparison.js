fb51982fad57103709cb97b59dd06dfa27ee0d20
Rachel Eiting <racheleiting@rachels-mac-pro.attlocal.net>
2018-08-26 15:43:03 -0500

    comparison page now generates dynamic table

--- comparison.js
+++ comparison.js
@@ -2,21 +2,25 @@ $(document).ready(function() {
 
     //gets the JSON string stored in local storage and sets it to myStorage
     var myStorage = window.localStorage;
-    //console.log(myStorage);
     //takes the JSON from the storage and parses it so it will be an object and sets it to parsedArray
     var parsedArray = JSON.parse(myStorage.formArray);
-    console.log(parsedArray);
+    //console.log(parsedArray);
     //iterates through the array of objects and has a function with a counter (iterator) and the object
-    $.each( parsedArray, function( i, val ) {
-        
-        //set the text of the id of name + 1 (because the array is 0 indexed) to be the value of prodvalue
-        $('#name' + (i+1) ).append( val.name);
-        //set the text of the id of desc + 1 (because the array is 0 indexed) to be the value of descvalue
-        $( '#desc' + (i+1) ).text( val.description );
-        //set the text of the id of price + 1 (because the array is 0 indexed) to be the value of pricevalue
-        $( '#price' + (i+1) ).text( val.price );
-      });  
-      
+    $('#table-container').append('<table />');
+    
+        $.each(parsedArray, function(i, val) {
+            var row = $('<tr>').append(
+                $('<td>').text(val.name),
+                $('<td>').text(val.description),
+                $('<td>').text(val.price)
+            ).appendTo('#table-container table');
+
+            console.log(val.name);
+            console.log(val.description);
+            console.log(val.price);
+        })
+
+    
       //on button click
       $('button').click(function(){
           //set var min to be the lowest price value
